6cc06eb40bc059b755df67ef5fda38bf6ba08d70
Milo0128 <camilo_0128@hotmail.com>
2022-06-23 02:00:59 -0500

    Arreglar imagenes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path =require('path');
 const publicPath = path.resolve(__dirname,"./public")
 
-app.use("/static",express.static(publicPath));
+app.use(express.static(publicPath));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
