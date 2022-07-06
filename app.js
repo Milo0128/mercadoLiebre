d272661c766806518997506d0551ed3c9f97b454
Milo0128 <camilo_0128@hotmail.com>
2022-07-05 21:58:03 -0500

    Pagina Register

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
 
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
 
 app.listen (3000, () => {
     console.log('servidor iniciado en: http://localhost:3000');
